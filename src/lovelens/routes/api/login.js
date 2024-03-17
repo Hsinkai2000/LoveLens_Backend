80f4b312f56c9069774992c46e06ebc7b313ee67
Hsin Kai <hsinkai2000@gmail.com>
2024-03-17 20:23:53 +0800

    login with email/password

--- src/lovelens/routes/api/login.js
+++ src/lovelens/routes/api/login.js
@@ -1,39 +1,59 @@
-
-const {check, validationResult} = require('express-validator');
-const { getAuth, createUserWithEmailAndPassword } = require("firebase/auth");
-const express = require('express');
+const { check, validationResult } = require("express-validator");
+const {
+  getAuth,
+  signInWithEmailAndPassword,
+  createUserWithEmailAndPassword,
+} = require("firebase/auth");
+const express = require("express");
 const router = express.Router();
 const auth = getAuth();
 
-router.post('/',[
-        check("email", "Email is not in the correct format").isEmail(),
-        check("password", "Password must be more than 5 characters").isLength({min:5})
-    ], async (req,res)=>{
-        const errors = validationResult(req);
-        if (!errors.isEmpty()){
-            return res.status(400).json({errors: errors.array()});
-        }
-    
-        const {email, password} = req.body;
-        
-        try{
-            createUserWithEmailAndPassword(auth, email, password)
-            .then((userCredential) => {
-              // Signed up 
-              const user = userCredential.user;
-              var idToken = user.getIdToken();
+router.post(
+  "/",
+  [
+    check("email", "Email is not in the correct format").isEmail(),
+    check("password", "Password must be more than 5 characters").isLength({
+      min: 5,
+    }),
+  ],
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
 
-            
+    const { email, password } = req.body;
+    try {
+      createUserWithEmailAndPassword(auth, email, password)
+        .then((userCredential) => {
+          // Signed up
+          const user = userCredential.user;
 
-              return res.status(200).json({"User": user, "idToken": idToken});
-            })
-            .catch((error) => {
-              const errorMessage = error.message;
-              return res.status(400).json({"errors": errorMessage});
-            });
-        } catch(error){
-            res.status(500).send(error);
-        }
-    })
+          return res.status(200).json({ Status: "signup", User: user });
+        })
+        .catch((error) => {
+          if (error.code == "auth/email-already-in-use") {
+            try {
+              signInWithEmailAndPassword(auth, email, password)
+                .then((userCredential) => {
+                  const user = userCredential.user;
+
+                  return res.status(200).json({ Status: "signin", User: user });
+                })
+                .catch((error) => {
+                  const errorMessage = error.message;
+                  return res.status(400).json({ errors: errorMessage });
+                });
+            } catch (error) {
+              return res.status(500).send(error.code);
+            }
+          }
+        });
+    } catch (error) {
+      console.log("HELLO");
+      return res.status(500).send(error.message);
+    }
+  }
+);
 
 module.exports = router;
--- src/lovelens/server.js
+++ src/lovelens/server.js
@@ -38,8 +38,8 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
-app.use("/api/register", require("./routes/api/register.js"));
-app.use("/api/register", require("./routes/api/register.js"));
+// app.use("/api/register", require("./routes/api/register.js"));
+app.use("/api/login", require("./routes/api/login.js"));
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
