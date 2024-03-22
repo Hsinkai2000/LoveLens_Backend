bfff358e20ec650574d393531c43c6bf29bd7170
Hsin Kai <hsinkai2000@gmail.com>
2024-03-22 11:51:30 +0800

    add mongoose

--- src/lovelens/routes/api/create_room.js
+++ src/lovelens/routes/api/create_room.js
@@ -8,15 +8,33 @@ const {
 } = require("firebase/auth");
 var { verifyID } = require("../../controller/fbAuth.js");
 const fbAuth = require("../../controller/fbAuth.js");
+const Rooms = require("../../models/room_schema.js");
 const auth = getAuth();
 
-router.post("/", fbAuth, function (req, res, next) {
+function getRandomInt() {
+  return Math.floor(Math.random() * 9999);
+}
+
+router.post("/", fbAuth, function async (req, res, next) {
   console.log("LALALALALALA in create room" + req.user);
   var { roomName } = req.body;
-  var { accessToken } = req.header;
-
   if (req.user) {
-    res.status(200).json({ token: req.user });
+    var newRoom = new Rooms({
+      name: roomName,
+      room_code: getRandomInt(),
+      owner: req.user,
+      pictures: [],
+      num_of_pics: 0,
+      creation_date: Date.now()
+    })
+
+    try{
+      newRoom.save();
+      res.status(200).json({ room: newRoom });
+    }catch (error){
+      res.status(500).json({error: error.message});
+    }    
+
   }
 });
 
