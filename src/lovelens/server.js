363e34e1678810954d388a4476ea90697af3cca3
Hsin Kai <hsinkai2000@gmail.com>
2024-03-22 12:55:12 +0800

    gitignore in main folder

--- src/lovelens/server.js
+++ src/lovelens/server.js
@@ -6,15 +6,14 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
-const {getAuth} = require("firebase/auth");
-const fbAuth = require("./controller/fbAuth.js");
-const {firebase, admin} = require("./config/fbConfig.js");
+const { getAuth } = require('firebase/auth');
+const fbAuth = require('./controller/fbAuth.js');
+const { firebase, admin } = require('./config/fbConfig.js');
 const { mongooseRun } = require('./config/mongoConfig.js');
 const { roomRoutes } = require('./routes/api/room/room_routes.js');
-const {setRoutes} = require('./routes_main.js');
+const { setRoutes } = require('./routes_main.js');
 
 mongooseRun().catch(console.dir);
-
 
 var app = express();
 
@@ -31,49 +30,58 @@ app.use(express.static(path.join(__dirname, 'public')));
 setRoutes(app);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
+app.use(function (req, res, next) {
+    next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+app.use(function (err, req, res, next) {
+    // set locals, only providing error in development
+    res.locals.message = err.message;
+    res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+    // render the error page
+    res.status(err.status || 500);
+    res.render('error');
 });
 
-function print (path, layer) {
-  if (layer.route) {
-    layer.route.stack.forEach(print.bind(null, path.concat(split(layer.route.path))))
-  } else if (layer.name === 'router' && layer.handle.stack) {
-    layer.handle.stack.forEach(print.bind(null, path.concat(split(layer.regexp))))
-  } else if (layer.method) {
-    console.log('%s /%s',
-      layer.method.toUpperCase(),
-      path.concat(split(layer.regexp)).filter(Boolean).join('/'))
-  }
+function print(path, layer) {
+    if (layer.route) {
+        layer.route.stack.forEach(
+            print.bind(null, path.concat(split(layer.route.path)))
+        );
+    } else if (layer.name === 'router' && layer.handle.stack) {
+        layer.handle.stack.forEach(
+            print.bind(null, path.concat(split(layer.regexp)))
+        );
+    } else if (layer.method) {
+        console.log(
+            '%s /%s',
+            layer.method.toUpperCase(),
+            path.concat(split(layer.regexp)).filter(Boolean).join('/')
+        );
+    }
 }
 
-function split (thing) {
-  if (typeof thing === 'string') {
-    return thing.split('/')
-  } else if (thing.fast_slash) {
-    return ''
-  } else {
-    var match = thing.toString()
-      .replace('\\/?', '')
-      .replace('(?=\\/|$)', '$')
-      .match(/^\/\^((?:\\[.*+?^${}()|[\]\\\/]|[^.*+?^${}()|[\]\\\/])*)\$\//)
-    return match
-      ? match[1].replace(/\\(.)/g, '$1').split('/')
-      : '<complex:' + thing.toString() + '>'
-  }
+function split(thing) {
+    if (typeof thing === 'string') {
+        return thing.split('/');
+    } else if (thing.fast_slash) {
+        return '';
+    } else {
+        var match = thing
+            .toString()
+            .replace('\\/?', '')
+            .replace('(?=\\/|$)', '$')
+            .match(
+                /^\/\^((?:\\[.*+?^${}()|[\]\\\/]|[^.*+?^${}()|[\]\\\/])*)\$\//
+            );
+        return match
+            ? match[1].replace(/\\(.)/g, '$1').split('/')
+            : '<complex:' + thing.toString() + '>';
+    }
 }
 
-app._router.stack.forEach(print.bind(null, []))
+app._router.stack.forEach(print.bind(null, []));
 
 module.exports = app;
