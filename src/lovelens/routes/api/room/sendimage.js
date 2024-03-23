ef690f6dae2a7a41f5516997687788467f52dd62
Ning Hui <ninghuiker@gmail.com>
2024-03-23 16:11:07 +0800

    image can be uploaded

--- src/lovelens/routes/api/room/sendimage.js
+++ src/lovelens/routes/api/room/sendimage.js
@@ -1,50 +1,89 @@
-var express = require("express");
+var express = require('express');
 var router = express.Router();
-const {
-  getAuth,
-  verifyIdToken,
-} = require("firebase/auth");
-var { verifyID } = require("../../../controller/fbAuth.js");
-const fbAuth = require("../../../controller/fbAuth.js");
-const Images = require("../../../models/image_schema.js");
-const { firebase } = require("../../../config/fbConfig.js");
-const {ref, getStorage} = require("firebase/storage");
+const { getAuth, verifyIdToken } = require('firebase/auth');
+var { verifyID } = require('../../../controller/fbAuth.js');
+const fbAuth = require('../../../controller/fbAuth.js');
+const Images = require('../../../models/image_schema.js');
+const { firebase } = require('../../../config/fbConfig.js');
+const { ref, getStorage } = require('firebase/storage');
 const auth = getAuth();
+var multer = require('multer');
+// var upload = multer({
+//     dest: './routes/api/room/uploads/',
+//     filename: file.originalname
+// });
+
+var storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+        cb(null, './upload');
+    },
+    filename: function (req, file, cb) {
+        cb(null, file.originalname);
+    }
+});
+var upload = multer({ storage: storage });
+
+var fs = require('fs');
 //const wedpic = require("./wed1.png");
 
 function getRandomInt() {
-  return Math.floor(Math.random() * 9999);
+    return Math.floor(Math.random() * 9999);
 }
 
-router.post("/", fbAuth, function async (req, res, next) {
-    console.log("start sending image" + req.user);
-    var { image } = req.body;
-    const selectedFile = image
-    const storage = getStorage()
-    const storageRef = ref(storage)
-    const fileRef = storageRef.child(selectedFile)
+router.post(
+    '/',
+    fbAuth,
+    upload.single('image'),
+    function async(req, res, next) {
+        // var { image } = req.file;
+        console.log('here');
+        var response = '<a href="/">Home</a><br>';
+        response += 'Files uploaded successfully.<br>';
+        response += `<img src="${req.file.path}" /><br>`;
+        //return res.send(response);
 
-    fileRef.put(selectedFile).then((snapshot)=>{
-        snapshot.ref.getDownloadURL().then((downloadURL)=>{
+        const selectedFile = req.file;
+
+        const storage = getStorage();
+        const fileRef = ref(storage, req.file.path);
+        console.log("Fileref: " + fileRef)
+
+        fileRef.put(selectedFile).then((snapshot) => {
+          snapshot.ref.getDownloadURL().then((downloadURL) => {
             console.log(downloadURL);
             setImgUrl(downloadURL);
-        })
-    })
-//   console.log("start sending image" + req.user);
-//   var { imageUrl } = req.body;
-//   if (req.user) {
-//     var newImage = new Images({
-//       imageURL: imageUrl
-//     })
+          });
+        });
 
-//     try{
-//       newImage.save();
-//       res.status(200).json({ room: newImage });
-//     }catch (error){
-//       res.status(500).json({error: error.message});
-//     }    
+        return "into firebase liao"
 
-//   }
-});
+
+        // const selectedFile = image;
+        // const storage = getStorage();
+        // const fileRef = ref(storage, "/images/hi.png");
+
+        // fileRef.put(selectedFile).then((snapshot) => {
+        //   snapshot.ref.getDownloadURL().then((downloadURL) => {
+        //     console.log(downloadURL);
+        //     setImgUrl(downloadURL);
+        //   });
+        // });
+        //   console.log("start sending image" + req.user);
+        //   var { imageUrl } = req.body;
+        //   if (req.user) {
+        //     var newImage = new Images({
+        //       imageURL: imageUrl
+        //     })
+
+        //     try{
+        //       newImage.save();
+        //       res.status(200).json({ room: newImage });
+        //     }catch (error){
+        //       res.status(500).json({error: error.message});
+        //     }
+
+        //   }
+    }
+);
 
 module.exports = router;
