af334da7c8442047c72be9d72214aaf955299f71
Hsin Kai <hsinkai2000@gmail.com>
2024-04-12 22:33:08 +0800

    including imageRoutes in routes_main

--- src/lovelens/routes_main.js
+++ src/lovelens/routes_main.js
@@ -3,12 +3,13 @@ var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users.js');
 var loginRouter = require('./routes/api/login.js');
 var { roomRoutes } = require('./routes/api/room/room_routes.js');
+const { imageRoutes } = require('./routes/api/image/image_routes.js');
 
 const setRoutes = (app) => {
     app.use('/', indexRouter);
     app.use('/users', usersRouter);
     app.use('/api/login', require('./routes/api/login.js'));
-    app.use('/api/image/upload', require('./routes/api/image/upload.js'));
+    imageRoutes(app);
     roomRoutes(app);
 };
 
